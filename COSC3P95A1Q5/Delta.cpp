e226d87b318c37af7393d19edcff81906008fdd2
Daniel Yang <danielyr123001@gmail.com>
2023-10-16 18:54:22 -0400

    Changes

--- COSC3P95A1Q5/Delta.cpp
+++ COSC3P95A1Q5/Delta.cpp
@@ -2,20 +2,26 @@
 #include <string>
 
 #include <cctype>
-//using namespace std
+
 
 bool check(std::string input,std::string output){
-    bool hasUpperCase = false;
+   std::string expected_output = "";
     for (char ch : input) {
         if (std::isupper(ch)) {
-            ch = std::tolower(ch); // 
-            hasUpperCase = true;
+            expected_output += std::tolower(ch);
+            
+        } else if(std::isdigit(ch)) {
+            expected_output += ch;
+            
+        } else{
+             expected_output += std::toupper(ch);
+            
         }
     }
 
-    // 
+     
     if (input == output) {
-        return !hasUpperCase; // same output and without upper case
+        return true; //pass
     } else {
         return false; // fail
     }
@@ -24,51 +30,72 @@ bool check(std::string input,std::string output){
 
 
 
+std::string test(std::string input) {
+    std::string output = "";
+    for (char ch : input) {
+        if (std::isupper(ch)) {
+            output += std::tolower(ch);
+            
+        } else if(std::isdigit(ch)) {
+            output += ch;
+            
+        } else{
+             output += std::toupper(ch);
+            
+        }
+    }
+    
+    //std::cout << output <<std::endl;
+    return output;
+   
+}
 
 
-
-void deltadebug(std::string input){
+int deltadebug(std::string input){
     int length= input.length();
-    int location=length/2;
+    int location;
     std::string test_part;
+    std::string output;
+
+    location=length/2;
+    output=test(input);
+     std::cout << output <<std::endl;
 
     while(location>=1){
-        for(int i=0; i<=location;i++){
-            test_part=input.substr(0,location);
-             check(input,processString(input));
+      test_part=input.substr(0,location);
+             check(test_part,test(input));
             
-            if( check(input,processString(input))==false){
+            if( check(input,test(input))==false){
+                location=location/2;
 
+                 }else{
 
-
+                test_part = input.substr(location,length);
+                location = (length-location) / 2;
             }
 
 
 
         }
 
+  std::cout << location <<std::endl;
 
 
-    }
+ if(location!=0){
+    std::cout <<"The test find bugs and locations are";
+   std::cout << location <<std::endl;
+ }else{
 
+    std::cout << "The test does not find bugs" <<std::endl;
+ }
+return location;
 }
-
-std::string processString(std::string input) {
-    std::string output = "";
-    for (char ch : input) {
-        if (std::isupper(ch)) {
-            output += std::tolower(ch);
-        } else if(std::isdigit(ch)) {
-            output += ch;
-        } 
-    }
-    return output;
-}
-
 
 
 int main() {
-    // Test the processString function with C++ code
-    //2023
+  
+     std::string input = "a1b2";
+    test(input);
+    deltadebug(input);
     return 0;
 }
